e56e657ac7e97672329853af7bbf345d1d107ac3
catalinaIlabaca <catalina.a.ilabaca@uchile.cl>
2024-11-14 17:36:27 -0300

    clean code and update variables names

--- course_classification/tests.py
+++ course_classification/tests.py
@@ -1,23 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from mock import patch, Mock, MagicMock
-from collections import namedtuple
 from django.urls import reverse
-from django.test import TestCase, Client
-from django.conf import settings
-from django.contrib.auth.models import Permission, User
-from django.contrib.contenttypes.models import ContentType
-from urllib.parse import parse_qs
+from django.test import Client
 import urllib.parse
-from opaque_keys.edx.locator import CourseLocator
-from xmodule.modulestore.tests.factories import CourseFactory, ItemFactory
-from xmodule.modulestore import ModuleStoreEnum
+from xmodule.modulestore.tests.factories import CourseFactory
 from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
-from common.djangoapps.student.tests.factories import CourseEnrollmentAllowedFactory, UserFactory, CourseEnrollmentFactory
-import json
+from common.djangoapps.student.tests.factories import UserFactory, CourseEnrollmentFactory
 from . import helpers
-from .views import CourseClassificationView
 from .models import MainCourseClassification, CourseClassification, MainCourseClassificationTemplate, CourseCategory
 from django.core.files.uploadedfile import SimpleUploadedFile
 from datetime import datetime
