292812e7ec3a41c74025d0a9f3a654d4f4b92db5
catalinaIlabaca <catalina.a.ilabaca@uchile.cl>
2025-04-09 11:51:04 -0400

    ci: add coverage to workflow test

--- .github/test.sh
+++ .github/test.sh
@@ -1,9 +1,12 @@
 #!/bin/dash
 
 pip install -e /openedx/requirements/app
+pip install pytest-cov genbadge[coverage]
 
 cd /openedx/requirements/app
 cp /openedx/edx-platform/setup.cfg .
+sed -i '/--json-report/c addopts = --nomigrations --reuse-db --durations=20 --json-report --json-report-omit keywords streams collectors log traceback tests --json-report-file=none --cov=course_classification/ --cov-report term-missing --cov-report xml:reports/coverage/coverage.xml --cov-fail-under 70' setup.cfg
+
 mkdir test_root
 cd test_root/
 ln -s /openedx/staticfiles .
@@ -11,3 +14,6 @@ ln -s /openedx/staticfiles .
 cd /openedx/requirements/app
 
 DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest course_classification/tests.py
+
+echo "[run]\nomit = course_classification/migrations/*" > .coverage
+genbadge coverage
