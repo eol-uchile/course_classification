26edebfbdcdf9d6a53bb0b79e6ff4fce0adbedae
catalinaIlabaca <catalina.a.ilabaca@uchile.cl>
2025-08-20 12:57:18 -0400

    feat: adds function to filter courses by course_state
    - refactor function desription
    - fix boolean feature parameter

--- course_classification/utils.py
+++ course_classification/utils.py
@@ -1,8 +1,6 @@
 # Python Standard Libraries
 import json
 import logging
-from collections import OrderedDict
-from .models import CourseClassification
 
 # Installed packages (via pip)
 from django.test.client import RequestFactory
@@ -11,11 +9,10 @@ from django.test.client import RequestFactory
 from .views import course_discovery_eol
 
 logger = logging.getLogger(__name__)
-   
 
 def get_course_ctgs():
     """
-        Return dict with categories and its courses
+        Return courses that are featured
     """
     factory = RequestFactory()
     data = {
@@ -34,3 +31,30 @@ def get_course_ctgs():
     results = json.loads(response.content)
     # Get courses from results
     return results["results"]
+
+def get_courses_filtered_by_course_state(course_state_list):
+    """
+        Return courses filter by course_state
+    """
+    factory = RequestFactory()
+    data = {
+        'search_string': '',
+        'order_by': '',
+        'year': '',
+        'state': '',
+        'classification': '',
+        'page_size': '20',
+        'page_index': '0'
+    }
+    # Request call using data parameters
+    request = factory.post('/course_classification/search/', data=data)
+    response = course_discovery_eol(request)
+    results = json.loads(response.content)
+    # Get courses from results
+    courses = results["results"]
+    # Filter courses by specific course_state 
+    courses_filtered = []
+    for course in courses:
+        if course["course_state"] in course_state_list:
+            courses_filtered.append(course)
+    return courses_filtered
