4ecb4d915bb72c2bae3d909194d5cd59f84670b9
catalinaIlabaca <catalina.a.ilabaca@uchile.cl>
2026-05-19 13:20:40 -0400

    feat: add initial settings

--- course_classification/settings/common.py
+++ course_classification/settings/common.py
@@ -1,2 +1,6 @@
 def plugin_settings(settings):
-    pass
+    settings.EXPLORE_COURSES_PAGE_SIZE = 20
+    settings.MAX_ELASTICSEARCH_PAGE_SIZE = 200
+    settings.COURSE_SEARCH_INITIAL_YEAR = 2020
+    settings.COURSE_SEARCH_FUTURE_TIME_HORIZON = 262080
+    
