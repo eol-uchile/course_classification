9d96d20da199098aaff2b9633f324a061cb94639
catalinaIlabaca <catalina.a.ilabaca@uchile.cl>
2026-05-19 15:13:46 -0400

    chore: update semantic version to 1.6.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="course_classification",
-    version="1.5.2",
+    version="1.6.0",
     author="Oficina EOL UChile",
     author_email="eol-ing@uchile.cl",
     description=".",
