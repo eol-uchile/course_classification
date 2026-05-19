f0153ed1289133e417bda89dc8e3655195d8bcb9
catalinaIlabaca <catalina.a.ilabaca@uchile.cl>
2026-05-19 13:21:16 -0400

    feat: add new end point get_initial_settings
    - return initial_year and time_horizon to front

--- course_classification/settings/common.py
+++ course_classification/settings/common.py
@@ -2,5 +2,5 @@ def plugin_settings(settings):
     settings.EXPLORE_COURSES_PAGE_SIZE = 20
     settings.MAX_ELASTICSEARCH_PAGE_SIZE = 200
     settings.COURSE_SEARCH_INITIAL_YEAR = 2020
-    settings.COURSE_SEARCH_FUTURE_TIME_HORIZON = 262080
+    settings.COURSE_SEARCH_FUTURE_TIME_HORIZON = 262080 #approx 6 month
     
